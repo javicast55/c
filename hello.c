86f1906756972783cf94422386b7ff0f12135ff8
javicast55 <javieer55c@gmail.com>
2024-12-18 09:59:55 +0000

    Traslate question from eng to sp

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 int main (void)
 {
     char name[] = ""; 
-    printf("What's your name?: ");
+    printf("Cual es tu nombre?: ");
     scanf("%s", &name);
-    printf("!!Hello %s, the best programer!!", name);
+    printf("!!Hola %s, el mejor programador nunca antes visto!!", name);
 }
